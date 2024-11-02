ab077c7a337aed373939e609944706a189e2a078
Orkhand4 <orkhand4@mail.ru>
2024-11-02 11:40:13 +0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.scss'
+
+const App = () => {
+  return (
+    <>
+      <div className="box">
+        <h2>Hello World!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, dolorum!</p>
+        <div className="demo">
+          <h2>DEMO H2</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
